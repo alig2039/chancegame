093da1abc522784dfe42af9192e359185a60f9e9
alig2039 <alig2039@yahoo.com>
2022-08-19 08:20:56 +0000

    Added JavaScript file containing all game variables

---
+++ assets/js/game.js
@@ -0,0 +1,19 @@
+// The Variables
+
+let cover1 = document.querySelector(".cover1");
+let cover2 = document.querySelector(".cover2");
+let cover3 = document.querySelector("cover2");
+let play = document.querySelector(".play");
+
+let comscore = 0;
+let youscore = 0;
+let comScore = document.querySelector("#textcom1");
+let youScore = document.querySelector("#textyou1");
+let comresult = document.querySelector("#comresult");
+let youresult = document.querySelector("#youresult"); 
+let result = document.querySelector("#result");
+let rock = document.querySelector(".brock");
+let papper = document.querySelector(".bpapper");
+let scissors = document.querySelector(".bscissors");
+let spock = document.querySelector(".bspock");
+let lizard = document.querySelector(".blizard");
